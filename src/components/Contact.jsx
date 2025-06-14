73e85fa5d94d71296c0db1e65efc7d78b832cb6d
Harshad Vilas Magadum <hvmagadum04@gmail.com>
2025-06-14 11:55:43 +0530

    commented motion library

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,4 +1,4 @@
-import { motion } from 'framer-motion'
+// import { motion } from 'framer-motion'
 import { EnvelopeIcon, PhoneIcon, MapPinIcon } from '@heroicons/react/24/outline'
 import { useState, useRef } from 'react'
 import emailjs from '@emailjs/browser'
